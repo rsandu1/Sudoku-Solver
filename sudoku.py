140c749799e9c97629e425bc5ff5b192f3c3b2fb
rsandu1 <107316441+rsandu1@users.noreply.github.com>
2024-01-18 14:00:23 -0500

    Create sudoku.py
    Outputs Sudoku Board

---
+++ sudoku.py
@@ -0,0 +1,26 @@
+#Sudoku Board some 9x9 2D array
+board = [
+    [1,0,0,4,8,9,0,0,6],
+    [7,3,0,0,0,0,0,4,0],
+    [0,0,0,0,0,1,2,9,5],
+    [0,0,7,1,2,0,6,0,0],
+    [5,0,0,7,0,3,0,0,8],
+    [0,0,6,0,9,5,7,0,0],
+    [9,1,4,6,0,0,0,0,0],
+    [0,2,0,0,0,0,0,3,7],
+    [8,0,0,5,1,2,0,0,4]
+]
+
+def printBoard(board):
+    for i in range(len(board)):
+        if i % 3 ==0 and i != 0:
+            print("-------------------------")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print (str(board[i][j]) + " ", end="")
+
+printBoard(board)
