7563edc26714bbb6a94f6d3255087817a96a9d7f
rsandu1 <107316441+rsandu1@users.noreply.github.com>
2024-03-21 15:15:18 -0400

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -31,5 +31,53 @@ def findEmpty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j) 
+    return None
 
-#printBoard(board)
+#Backtracking Algorithim
+def solve(board):
+    #Base Case
+    find = findEmpty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    #Recursive Case
+    for i in range(1,10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+#Checking to see if a given position in the board is valid
+#Returns a boolean
+def valid(board, number, position):
+    # Check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # Check box within board
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == number and (i,j) != position:
+                return False
+
+    return True
+printBoard(board)
+solve(board)
+print("___________________________________")
+printBoard(board)
